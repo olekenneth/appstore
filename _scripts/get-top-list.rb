5a85d43619f6fa08a3fd50bc61617fa26a38b41a
Ole-Kenneth Bratholt <ok@rait.no>
2021-09-27 14:10:19 +0200

    Add get-top-list from Apple

---
+++ _scripts/get-top-list.rb
@@ -0,0 +1,15 @@
+require 'net/http'
+require 'json'
+require './_scripts/search.rb'
+
+url = "https://rss.itunes.apple.com/api/v1/no/ios-apps/top-free/all/100/explicit.json"
+puts url
+uri = URI(url)
+response = Net::HTTP.get(uri)
+data = JSON.parse(response)['feed']
+
+for i in 0..data['results'].length - 1
+  app = data['results'][i]
+  id = app['id']
+  search(id, '--y')
+end
