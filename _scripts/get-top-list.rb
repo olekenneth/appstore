5aec96840a31bb0911ffe53a75bf8658054ce16e
Ole-Kenneth Bratholt <ok@rait.no>
2021-12-16 17:45:18 +0100

    Update get top list script

--- _scripts/get-top-list.rb
+++ _scripts/get-top-list.rb
@@ -7,8 +7,8 @@ uri = URI(url)
 response = Net::HTTP.get(uri)
 data = JSON.parse(response)['feed']
 
-for i in 0..data['feed']['results'].length - 1
-  app = data['feed']['results'][i]
+for i in 0..data['results'].length - 1
+  app = data['results'][i]
   id = app['id']
   search(id, '--y')
 end
