cf0f2138beffa4f6ae3b008d58822570ddc425d0
Ole-Kenneth Bratholt <ok@schibsted.com>
2025-02-03 13:42:43 +0100

    Update JSON feed

--- _scripts/get-top-list.rb
+++ _scripts/get-top-list.rb
@@ -2,7 +2,7 @@ require 'net/http'
 require 'json'
 require './_scripts/search.rb'
 
-url = "https://rss.applemarketingtools.com/api/v2/no/apps/top-free/30/apps.json"
+url = "https://rss.marketingtools.apple.com/api/v2/no/apps/top-free/30/apps.json"
 uri = URI(url)
 response = Net::HTTP.get(uri)
 data = JSON.parse(response)['feed']
